1a72bd3490e5e4bbaf1f351a310429d6c95759e1
Arthur Soulié <arthur.soulie@tuta.io>
2021-12-07 23:31:56 +0100

    Add basic app example
    Notes:
    * Example found on arango official guide for arangojs

--- app/app.js
+++ app/app.js
@@ -1,5 +1,24 @@
-const main = () => {
-    console.log("Hello World")
+import { Database, aql } from "arangojs";
+
+const connect = async (db) => {
+    const now = Date.now()
+    try {
+        const cursor = await db.query(aql`
+            RETURN${now}
+        `)
+        const result = await cursor.next()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const main = async () => {
+    console.log("Hello World!")
+    const db = new Database({
+        url: "http://default:8529",
+        databaseName: "my_database",
+    })
+    await connect(db)
 }
 
 main()
